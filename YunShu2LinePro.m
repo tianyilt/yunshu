d16d21fbcff7d82092c8cc53497e093eb09248a6
tianyilt <tianyilt@qq.com>
2019-08-06 18:05:01 +0800

    完成运输问题求解框架

--- YunShu2LinePro.m
+++ YunShu2LinePro.m
@@ -1,10 +1,12 @@
-function [solve,optim]=YunShu2LinePro(Yun,Xv,Chan)
+function [solution,optim]=YunShu2LinePro(Yun,Xv,Chan)
 %YUNSHU2LINEPRO 能够在建立完成运输问题表上作业法的表格后，直接方便求解运输问题。
 %而不用去为了迎合线性规划工具箱而自己构造参数
 %   输入：
 % Yun，是一个销地数*产地数的矩阵，i行j列元素表示从产地j运输到销售地i的价格
 % Xv，是一个列向量，第i个元素表示第i个需求地需求
 % Chan，是一个列向量，第i个元素表示第i个生产地产出
+%   输出：是一个销地数*产地数的矩阵，i行j列元素表示从产地j运输到销售地i的运输量
+% solution，
 f=[];
 for i=1:size(Yun,1)
     f=[f,Yun(i,:)];
@@ -13,11 +15,11 @@ end
 b=[Xv;Chan]';
 A=zeros(length(b),size(Yun,1)*size(Yun,2));
 dim1=size(A,1);%行数，而且是产地+销售地
-dim1_Yun0=size(Yun,1);%行数
-dim2_Yun0=size(Yun,2);%列数
+dim1_Yun=size(Yun,1);%行数
+dim2_Yun=size(Yun,2);%列数
 for i=1:dim1
     if i<=length(Xv)
-        A(i,dim2_Yun*i-dim2_Yun0+1:dim2_Yun*i)=1;
+        A(i,dim2_Yun*i-dim2_Yun+1:dim2_Yun*i)=1;
     else
         for j=1:dim1_Yun
             A(i,(i-length(Xv))+(j-1)*dim2_Yun)=1;
@@ -27,5 +29,10 @@ end
 lb=zeros(1,size(Yun,1)*size(Yun,2));
 ub=ones(1,size(Yun,1)*size(Yun,2))*inf;
 [solve,optim]=linprog(f,[],[],A,b,lb,ub);
+solution=[];
+for i=1:size(Yun,1)
+    solution=[solution,solve(size(Yun,2)*i-size(Yun,2)+1:size(Yun,2)*i)];
+end
+solution=solution';
 end
 
